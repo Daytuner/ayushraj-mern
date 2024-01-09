4c4c6961680e6e3d80316b12bd4355e3ecea0339
Ayush Raj Panda <apanda107@yahoo.com>
2024-01-09 21:45:26 +0530

    paypal button addition

--- frontend/src/screens/Order.jsx
+++ frontend/src/screens/Order.jsx
@@ -43,6 +43,45 @@ const Order = () => {
   },[order,paypal,paypalDispatch,loadingPayPal,errorPayPal])
 
 
+  function onApprove(data,actions){
+   return actions.order.capture().then(async function(details){
+      try {
+         await payOrder({orderId,details})
+         refetch();
+         toast.success("Payment Successful")
+      } catch (error) {
+         toast.error(error?.data?.message||error.message)
+      }
+   })
+  }
+
+  async function onApproveTest(){
+   await payOrder({orderId,details:{payer:{"email_address":'asdas@asdf'}}})
+    refetch()
+    toast.success("Payment Successful")
+  }
+
+  function onError(error){
+   toast.error(error.message)
+  }
+
+  function createOrder(data,actions){
+   return actions.order.create({
+      purchase_units:[
+         {
+            amount:{
+               value:order.totalPrice,
+               
+            },
+         },
+      ],
+   }).then((orderId)=> {
+      return orderId
+   })
+  }
+
+
+
     
   return  isLoading ? (<Loader/>) : error ? (<Message varient='danger'/>): (
      <>
@@ -137,6 +176,21 @@ const Order = () => {
                             <Col>${order.totalPrice}</Col>
                         </Row>
                     </ListGroup.Item>
+
+                        {!order.isPaid &&(
+                           <ListGroup.Item>
+                              {loadingPay && <Loader/>}
+                              {isPending?<Loader/>:(
+                                 <div>
+                                    <Button onClick={onApproveTest} style={{marginBottom:'10px'}}>Test Button</Button>
+                                    <div>
+                                       <PayPalButtons createOrder={createOrder} onApprove={onApprove} onError={onError} ></PayPalButtons>
+                                    </div>
+                                 </div>
+                              )}
+                           </ListGroup.Item>
+                        )}
+
                     </ListGroup>
             </Card>
          </Col>
